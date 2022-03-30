7c33cacaedb8d0648401f3965b4377826b811b00
ghanemgit <ghanem_97@outlook.com>
2022-03-30 11:54:24 +0300

    spring initializer

---
+++ src/test/java/com/mycode/codefellowship/CodeFellowShipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycode.codefellowship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeFellowShipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
