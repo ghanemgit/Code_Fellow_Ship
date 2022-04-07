ae1cb559d062c51e9cfc369ca9ae9e64c34fc3a5
ghanemgit <ghanem_97@outlook.com>
2022-04-07 03:01:44 +0300

    lab18 done

--- src/main/java/com/mycode/codefellowship/Controllers/PostController.java
+++ src/main/java/com/mycode/codefellowship/Controllers/PostController.java
@@ -1,7 +1,6 @@
 package com.mycode.codefellowship.Controllers;
 
 
-import com.mycode.codefellowship.Models.ApplicationUser;
 import com.mycode.codefellowship.Models.Post;
 import com.mycode.codefellowship.Repository.ApplicationUserRepository;
 import com.mycode.codefellowship.Repository.PostRepository;
@@ -9,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
